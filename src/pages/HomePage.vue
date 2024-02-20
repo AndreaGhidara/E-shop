b3b5ccf8a44498c3dac639ea5d0df02b24992d6d
Andrea Ghidara <Ghidara.andrea.99@gmail.com>
2024-02-20 13:30:31 +0100

    add footer

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import Footer from '../components/Footer/Footer.vue'
 import ProductTopList from '../components/ProductTopList/ProductTopList.vue'
 import Banner from '../components/Banner/Banner.vue'
 import Hero from '../components/Hero/Hero.vue'
@@ -8,9 +9,18 @@ import Hero from '../components/Hero/Hero.vue'
 
 <template>
     <section>
-        <Hero />
-        <Banner />
-        <ProductTopList />
+        <div class="py-10">
+            <Hero />
+        </div>
+        <div class="py-10">
+            <Banner />
+        </div>
+        <div class="py-10">
+            <ProductTopList />
+        </div>
+        <div class="pt-10">
+            <Footer></Footer>
+        </div>
     </section>
 </template>
 
