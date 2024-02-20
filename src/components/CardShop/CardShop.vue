4b97ed8bf6820a1464978439e27625f84314b473
Andrea Ghidara <Ghidara.andrea.99@gmail.com>
2024-02-20 12:58:10 +0100

    add card

---
+++ src/components/CardShop/CardShop.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="flex flex-col gap-4 w-52">
+        <div class="h-32 w-full bg-green-300 rounded-xl">
+
+        </div>
+        <div class="flex flex-col gap-2">
+            <div className="w-28 rounded font-semibold">
+                18.50€
+            </div>
+            <div className="text-xl font-bold">
+                Nome Prodotto
+            </div>
+            <div className="font-mono">
+                Descrizione Breve
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<style scoped></style>
--- src/components/ProductTopList/ProductTopList.vue
+++ src/components/ProductTopList/ProductTopList.vue
@@ -1,15 +1,25 @@
 <script setup lang="ts">
+import CardShop from '../CardShop/CardShop.vue'
 import SkeletonCard from '../SkeletonCard/SkeletonCard.vue'
+
+const logged = false
 
 </script>
 
 <template>
     <div class="container mx-auto my-5">
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-            <div v-for="n in 9" class="flex justify-center items-center">
-                <SkeletonCard />
-            </div>
+
             
+                <div v-if="!logged" v-for="n in 9" class="flex justify-center items-center">
+                    <SkeletonCard />
+                </div>
+            
+                <div v-else v-for="n in 9" class="flex justify-center items-center">
+                    <CardShop />
+                </div>
+            
+
         </div>
     </div>
 </template>
